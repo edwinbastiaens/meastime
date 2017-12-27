cc15fb35caf5146d7757a2779241bebad036bd88
edwinbastiaens <edwinbastiaens@hotmail.com>
2017-12-27 18:33:01 +0100

    initial

---
+++ src/MeasTime.php
@@ -0,0 +1,21 @@
+<?php
+namespace Tolima;
+
+class MeasTime{
+    private static $startTime;
+    private static $stopTime;
+    public static function strt(){
+        self::$startTime = self::microtime_float();
+        return self::$startTime;
+    }
+
+    public static function stop(){
+        self::$stopTime = self::microtime_float();
+        return self::$stopTime - self::$startTime;
+    }
+
+    private static function microtime_float() {
+        list($usec, $sec) = explode(" ", microtime());
+        return ((float)$usec + (float)$sec);
+    }
+}
